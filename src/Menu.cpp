b55f8608386ca0f97b81ec1f1a834b037052efae
vivikunin <vivikunin@gmail.com>
2025-06-08 10:45:39 -0300

    Empece altaUsuario

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -135,6 +135,7 @@ void altaUsuario(){
         std::cout << "Documento: ";
         std::getline(std::cin, documento);
         //TODO: usuarioOk = ci->altaCliente(nickname, contrasena, nombre, email, apellido, documento);
+        usuarioOk = factory->getControladorUsuario()->altaCliente(nickname, contrasena, nombre, email, apellido, documento);
 
     }else if (tipoUsuario == 1){
         std::cout << "Direccion: ";
@@ -144,6 +145,7 @@ void altaUsuario(){
         std::cout << "Telefono: ";
         std::getline(std::cin, telefono);
         //TODO: usuarioOk = ci->altaInmobiliaria(nickname, contrasena, nombre, email, direccion, url, telefono);
+        usuarioOk = factory->getControladorInmobiliarias()->altaInmobiliaria(nickname, contrasena, nombre, email, apellido, documento);
 
     }else if (tipoUsuario == 2){
         std::cout << "Cuenta Bancaria: ";
